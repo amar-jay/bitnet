07195d1fe791a8f20ab909d5d487009bb30dabb5
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-03-16 21:56:04 +0300

    made it in batch sample-ish as in the paper. But still cannot figure out how to make it matched

--- bitnet/bitlinear.py
+++ bitnet/bitlinear.py
@@ -4,6 +4,9 @@ from torch._prims_common import Tensor
 import torch.nn.functional as F
 
 
+# Group Quantization and Normalization is a way to scale up model for large scale deployment
+# It does so by partitioning and parrallelizing the matrix multiplication operation through a process called group quantization
+# on what dimension are we to split the input, batch, right??
 
 class BitLinear(nn.Linear):
     """BitLinear is a linear layer with bit-wise operations
@@ -20,12 +23,14 @@ class BitLinear(nn.Linear):
         in_features: int,
         out_features: int,
         bias: bool = True,
-        bit: int = 1,
+        bit: int = 8,
+        n_groups: int = 1,
         device = None,
         dtype = None
     ):
         super(BitLinear, self).__init__(in_features, out_features, bias, device, dtype)
         self.bit = bit
+        self.n_groups = n_groups
         self.ln = nn.LayerNorm(in_features)
         self.eps = 1e-05 # the eps usually used is 1e-05
         #self.alpha = torch.sum(self.weight) / (self.weight.size(0) * self.weight.size(0))
@@ -35,18 +40,32 @@ class BitLinear(nn.Linear):
         self.Qb = 2 ** (bit - 1) 
 
     def forward(self, input):
-            # normalize, quantize, transform, dequantize
-            x = self.ln(input)
-            self.gamma = torch.norm(x, p=torch.inf) # torch.max() will do too I guess??
-            x_hat = self._quantize(x)
-            y = self._dequantize(F.linear(x_hat, self.binarized_weight, self.bias))
-            return y
+        input = self.ln(input)
+        B, T, C = input.size()
 
-    def _dequantize(self, x):
-        return x * self.beta * self.gamma / self.Qb
+        x_groups = torch.chunk(input, self.n_groups, dim=1)
 
-    def _quantize(self, x):
-        v = x * (self.Qb / self.gamma)
+        # normalize, quantize, transform, dequantize
+        gamma = torch.stack([torch.max(x) for x in x_groups], dim=0) # gammas for each group. This may not be the best way to do it
+        # self.gamma = torch.norm(x, p=torch.inf) # torch.max() will do too I guess??
+        
+        x_hat = torch.stack([self._quantize(x, gamma[i]) for i in range(self.n_groups)]).view(B, T, C)
+
+        y = F.linear(x_hat, self.binarized_weight, self.bias)
+        y = self._dequantize(y, gamma)
+        return y
+
+    def _dequantize(self, x, gamma):
+        return x * self.beta * gamma / self.Qb
+
+    def _quantize(self, x, gamma):
+        """
+        Quantizes input group 
+
+        x     -> input group 
+        gamma -> gamma of the input group
+        """
+        v = x * (self.Qb / gamma)
         w = -self.Qb + self.eps
         x = self.Qb - self.eps
         return torch.clip(v, w, x) 
@@ -61,11 +80,12 @@ class BitLinear(nn.Linear):
 
 
     def extra_repr(self):
-        return 'in_features={}, out_features={}, bias={}, bit={}'.format(
+        return 'in_features={}, out_features={}, bias={}, bit={}, n_groups={}'.format(
             self.in_features,
             self.out_features,
             self.bias,
-            self.bit
+            self.bit,
+            self.n_groups
         )
 
 if __name__ == "__main__":
